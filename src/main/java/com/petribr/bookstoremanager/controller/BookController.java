d754aa3beb3d8c21ff966864d3f80986059b7402
petribr <romulo.petri@gmail.com>
2021-04-17 10:58:28 -0300

    Creation

---
+++ src/main/java/com/petribr/bookstoremanager/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.petribr.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+	@GetMapping
+	public String hello() {
+		return "Hello world from petribr sistemas";
+	}
+}
---
+++ src/test/java/com/petribr/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petribr.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
