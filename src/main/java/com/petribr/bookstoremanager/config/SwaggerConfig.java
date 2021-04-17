9bb7cc28177313dfa4edef521461d59905373dda
petribr <romulo.petri@gmail.com>
2021-04-17 14:06:28 -0300

    adição do travis CI/CD 2

---
+++ src/main/java/com/petribr/bookstoremanager/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.petribr.bookstoremanager.config;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	private static final String BASE_PACKAGE = "com.petribr.bookstoremanager";
+	private static final String API_TITLE = "Petribr - Bookstore Manager Course";
+	private static final String API_DESCRIPTION = "Bookstore Manager API Professional";
+	private static final String API_VERSION = "1.0.0";
+	private static final String CONTACT_NAME = "Romulo Petri";
+	private static final String CONTACT_GITHUB = "https://github.com/petribr";
+	private static final String CONTACT_EMAIL = "petribr@gmail.com";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
+}
--- src/main/java/com/petribr/bookstoremanager/controller/BookController.java
+++ src/main/java/com/petribr/bookstoremanager/controller/BookController.java
@@ -4,12 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
 
+	@ApiOperation(value = "Return an example hello world.")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Sucess method return.")
+	})
 	@GetMapping
 	public String hello() {
-		return "Hello world from petribr sistemas";
+		return "Hello world from PETRIBR sistemas";
 	}
 }
